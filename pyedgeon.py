c946b260924b497338c5c890c61281b0da4ca2c0
Abraham Hmiel <abehmiel@gmail.com>
2017-03-07 22:55:45 -0800

    Update and rename edgeon.py to pyedgeon.py
    Add some additional comments for clarity, reorder a few variables and move the most important user-editable ones to the top

--- pyedgeon.py
+++ pyedgeon.py
@@ -4,19 +4,25 @@ from PIL import ImageDraw
 from PIL import ImageOps
 from numpy import sqrt
  
+# Primary user-editable settings
+################################
 illusion_text = "NPR COOL DAD ROCK"
+font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSans-ExtraLight.ttf"
+num_rotations = 6
+file_ext = ".png"
+
+# Additional settings
+################################
 text_color = (0, 0, 0)
 background_color = (255, 255, 255)
 img_side = 1024
- 
-font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSans-ExtraLight.ttf"
 charmax = 18
 font_size_guess = 7*(30-len(illusion_text))
 crop_width_x = 12
 crop_width_y = 3
-num_rotations = 6
 darkness_threshold = 108
  
+# Fail if sentence is too long (it looks ugly)
 if len(illusion_text) <= charmax:
     pass
 else:
@@ -51,6 +57,7 @@ for font_trial in range(font_size_guess-30, font_size_guess+30):
     else:
         break
  
+# Start drawing boxes
 raw_img = Image.new("RGB", img_size_text, background_color)
 draw = ImageDraw.Draw(raw_img)
 font = ImageFont.truetype(font_path, font_size)
@@ -75,6 +82,7 @@ for y in range(img.size[1]):
 circle_img.paste(img, (0, 0))
 pixdata2 = circle_img.load()
 
+# Stretch text vertically along the path of a circle
 for x in range(img_side):
     Ysize = 2 * sqrt((img_side / 2) ** 2 - (x - (img_side / 2)) ** 2)
     for y in range(img_side):
@@ -84,14 +92,17 @@ for x in range(img_side):
         if sqrt((x-img_side/2)**2 + (y-img_side/2)**2) >= img_side/2 - 2:
             pixdata2[x, y] = (255, 255, 255, 0)
  
+# Stamp text repeatedly in a circular manner
 for i in range(num_rotations):
     this_circle = circle_img.rotate(i*180/num_rotations)
     full_image.paste(this_circle, (0, 0), this_circle)
  
+# Turn alpha channel back to white
 pixdata = full_image.load()
 for y in range(full_image.size[1]):
     for x in range(full_image.size[0]):
         if pixdata[x, y] == (255, 255, 255, 0):
             pixdata[x, y] = (255, 255, 255, 255)
  
-full_image.save(illusion_text+".png")
+# Save as png
+full_image.save(illusion_text+file_ext)
